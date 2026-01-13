d3715c0b27f9d4702caa35b094a4b84b972a1740
james talamo <jamestalamo4@gmail.com>
2026-01-13 21:59:01 +0800

    white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       </head>
       <body>
         <div
-          className="min-h-screen relative bg-background">
+          className="min-h-screen relative bg-background text-text">
           {/* Top Navigation Bar */}
           <nav
             className="fixed top-0 left-0 right-0 h-16 flex items-center px-6 z-50 border-b border-lines bg-background">
