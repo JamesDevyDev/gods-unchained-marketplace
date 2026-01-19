6a4038ab7d160d2d731cb0acf0b153976a821173
james talamo <jamestalamo4@gmail.com>
2026-01-19 14:10:26 +0800

    chained x

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -42,19 +42,6 @@ const Footer = () => {
                                     </svg>
                                 </div>
                             </a>
-
-                            <a
-                                href="https://twitter.com"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className='hover:opacity-80 transition-opacity'
-                            >
-                                <div className='w-12 h-12 bg-black rounded-full flex items-center justify-center border border-gray-700'>
-                                    <svg className='w-6 h-6 text-white' fill='currentColor' viewBox='0 0 24 24'>
-                                        <path d='M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z' />
-                                    </svg>
-                                </div>
-                            </a>
                         </div>
 
                         <p className='text-gray-300 text-sm leading-relaxed'>
